157853a22fa31fc0a0d6eda69ee76550296219a2
denis <denis.mourier2@gmail.com>
2022-01-27 15:01:00 +0100

    page accueil + menu

--- tests/unit/dateApp.test.js
+++ tests/unit/dateApp.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // !!! jest N'A PAS BESOIN D'IMPORTS => provoque une erreur :
 // TypeError: (0 , _jest.default) is not a function
 // import test from 'jest'
@@ -6,16 +7,17 @@
 // import it from 'jest'
 
 // import dateApp from '@/utils/dateApp.js'
-import {default as dateApp} from '../../src/utils/dateApp'
+import { default as dateApp } from '../../src/utils/dateApp'
 import moment from 'moment';
 
 
 
+// eslint-disable-next-line no-undef
 test('VUE_APP_DATE updated', () => {
     process.env['VUE_APP_DATE'] = '2022-01-02';
     expect(process.env.VUE_APP_DATE).toBe('2022-01-02');
     // console.log("VUE_APP_DATE 2 : "  + process.env.VUE_APP_DATE)
-} )
+})
 
 
 // permet de regrouper plusieurs tests
@@ -26,16 +28,17 @@ describe('vue_app_date updated', () => {
             expect(process.env.vue_app_date).toBe('2022-01-02');
 
         }),
-    it('dateApp should return 2022-01-02',
-    () => {
-        process.env['vue_app_date'] = '2022-01-02';
-        expect(dateApp()).toBe('2022-01-02');
-    }),
-    it('dateApp should return toto',
-    () => {
-        process.env['vue_app_date'] = '';
-        expect(dateApp()).toBe('moment().format('YYYY-MM-DD')');
-    })
+        it('dateApp should return 2022-01-02',
+            () => {
+                process.env['vue_app_date'] = '2022-01-02';
+                expect(dateApp()).toBe('2022-01-02');
+            }),
+        it('dateApp should return toto',
+            () => {
+                process.env['vue_app_date'] = '';
+                expect(dateApp()).toBe(moment().format('YYYY-MM-DD'));
+
+            })
 })
 
 
