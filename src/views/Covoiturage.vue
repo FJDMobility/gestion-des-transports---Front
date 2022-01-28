0e041bfe5e3c811a8a31d61d1e35044dca29b806
denis <denis.mourier2@gmail.com>
2022-01-28 15:11:39 +0100

    pb avec affichage de la liste des covoiturages lorsque on clique sur rafraichir la page du navigateur domage :(

--- src/views/Covoiturage.vue
+++ src/views/Covoiturage.vue
@@ -1,13 +1,20 @@
 <template lang="">
     <div>
-        
+        <div>
+        <v-container>
+            <listeCovoiturage/>
+        </v-container>
+        </div>
     </div>
 </template>
 <script>
+import listeCovoiturage from "../components/listeCovoiturage.vue";
 export default {
-    
-}
+  name: "Covoiturage",
+  components: {
+    listeCovoiturage,
+  },
+};
 </script>
 <style>
-    
 </style>
