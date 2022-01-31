d467fd1081feac94e641db76e189b424372ff324
denis <denis.mourier2@gmail.com>
2022-01-31 18:03:52 +0100

    liste covoiturages + affichage détail !!! annul attention, annulation à finir définir gestion des MaJ : - store - base serveur

---
+++ src/components/CovoiturageDetail.vue
@@ -0,0 +1,40 @@
+<template lang="">
+    <div>
+        <h2>Détail</h2>
+        <v-data-table :headers="headers" :items="covoiturage">
+          <template v-slot:item.organisateur="{ item }">
+      <v-chip red>
+        {{ item.organisateur.prenom+"  "+item.organisateur.nom }}
+      </v-chip>
+    </template>
+
+        </v-data-table>
+    </div>
+</template>
+<script>
+export default {
+  name: "CovoiturageDetail",
+  props : {
+        covoiturage : {},
+    },
+  data() {
+    return {
+      headers: [
+        { text: "Organisateur", value: "organisateur" },
+        { text: "Marque", value: "voiture.marque" },
+        { text: "Modèle", value: "voiture.model" },
+        { text: "Immatriculation", value: "voiture.immatriculation" },
+        { text: "Nb de places", value: "voiture.nbrPlaces" },
+      ],
+    //   covoiturageAffichage: this.covoiturage,
+    };
+  },
+  methods: {
+      affichOrganisateur() {
+          return this.covoiturage[0].organisateur.prenom + "  " + this.covoiturage[0].organisateur.nom
+      }
+  },
+};
+</script>
+<style>
+</style>
--- src/services/serviceCovoiturageApi.js
+++ src/services/serviceCovoiturageApi.js
@@ -4,98 +4,43 @@
 
 // const urlcovoiturage = process.env.VUE_APP_URLCOVOITURAGE;
 
-let allcovoiturageuser = {
-    "covoiturages":
-        [{
-            "id": 1,
-            "dateDepart": "2022-02-15T8:00",
-            "voiture": {
-                "id": 1,
-                "marque": "renault",
-                "model": "twingo",
-                "nbrPlaces": 3,
-                "immatriculation": "1234 tt 25"
-            },
-            "participant": [{
-                "id": 2,
-                "nom": "Martin",
-                "prenom": "Gerard",
-                "mail": "gerard.martin@covoit.com"
-            },
-            {
-                "id": 3, 
-                "nom": "Durant",
-                "prenom": "michel",
-                "mail": "michel.durant@covoit.com"
-            },
-            {
-                "id": 1,
-                "nom": "Mourier",
-                "prenom": "Denis",
-                "mail": "denis.mourier@covoit.com"
-            }
-            ],
-            "villeDepart": "Nîmes",
-            "villeArrivee": "Montpellier",
-            "placesDisponibles": 3
-        },
-        {
-            "id": 2,
-            "dateDepart": "2022-02-16T8:00",
-            "voiture": {
-                "id": 1,
-                "marque": "renault",
-                "model": "clio",
-                "nbrPlaces": 2,
-                "immatriculation": "567 tt 25"
-            },
-            "participant": [{
-                "id": 3,
-                "nom": "Durant",
-                "prenom": "michel",
-                "mail": "michel.durant@covoit.com"
-            },
-            {
-                "id": 1,
-                "nom": "Mourier",
-                "prenom": "Denis",
-                "mail": "denis.mourier@covoit.com"
-            }],
-            "villeDepart": "Nîmes",
-            "villeArrivee": "Le grau du roi",
-            "placesDisponibles": 0,
-        },
-        {
-            "id": 3,
-            "dateDepart": "2022-02-17T8:00",
-            "voiture": {
-                "id": 1,
-                "marque": "renault",
-                "model": "clio",
-                "nbrPlaces": 2,
-                "immatriculation": "567 tt 25"
-            },
-            "participant": [{
-                "id": 1,
-                "nom": "Mourier",
-                "prenom": "Denis",
-                "mail": "denis.mourier@covoit.com"
-            }],
-            "villeDepart": "Nîmes",
-            "villeArrivee": "Uzes",
-            "placesDisponibles": 0,
-        }
-        ]
-};
-
 
 export default class serviceCovoiturageApi {
+
+
+    static getCovoiturageRepo() {
+        // return JSON.parse(localStorage.covoiturageRepo);
+        console.log("getCovoiturage repo");
+        return localStorage.getItem("covoiturageRepo");
+    }
+
+
+    static storeCovoiturageRepo(repo) {
+        // localStorage.covoiturageRepo = JSON.stringify(repo);
+        localStorage.setItem("covoiturageRepo", JSON.stringify(repo)) ;
+    }
+
+
     static getAll(userId) {
         console.log("covoiturage getAll pour userId : ", userId)
         // return axios.get(urlbq + "/all")
-        return JSON.stringify(allcovoiturageuser) ;
+        // return JSON.stringify(covoiturageRepo);
+        return this.getCovoiturageRepo();
     }
 
+    static annulerCovoiturage(id, userId) {
+        console.log("annulation du covoiturage : ", id);
+        let covoiturageRepo = JSON.parse(this.getCovoiturageRepo());
+        // let covoiturageRepo = this.getCovoiturageRepo();
+        console.log(covoiturageRepo.covoiturages[id]);
+
+        delete covoiturageRepo.covoiturages[id].participant[userId];
+        // covoiturage
+
+        console.log(covoiturageRepo.covoiturages[id]);
+        this.storeCovoiturageRepo(covoiturageRepo);
+
+    }
 
     // static create(client) {
     //     axios.create(urlbq, client);
