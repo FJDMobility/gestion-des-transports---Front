cae99e1028252d51d6a45754c244a9b6edd18f63
denis <denis.mourier2@gmail.com>
2022-02-10 18:52:00 +0100

    liste covoiturage et covoiturage resa OK fct boutons

--- src/components/CovoiturageParticipants.vue
+++ src/components/CovoiturageParticipants.vue
@@ -19,7 +19,7 @@ CovoiturageParticipants
     </div>
 </template>
 <script>
-import router from "../router";
+// import router from "../router";
 export default {  
   name: "CovoiturageParticipants",
 
